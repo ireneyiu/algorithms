2082884e3ecd63acd39cff827623d063f0badee2
ireneyiu <ireyiu@gmail.com>
2014-04-13 23:42:04 -0700

    Add solution for shortest FizzBuzz

---
+++ arrays-strings/shortest-fizz-buzz.rb
@@ -0,0 +1,22 @@
+=begin
+Write a program that prints (to STDOUT) the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
+
+The goal is to write the shortest code possible.
+
+=end
+
+def fizz_buzz(max)
+  1.upto(max) do |num|
+    if num % 15 == 0
+      puts "FizzBuzz"
+    elsif num % 3 == 0
+      puts "Fizz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    else
+      puts num
+    end
+  end
+end
+
+fizz_buzz(100)
