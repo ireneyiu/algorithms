9d1a57fd9266136f3d4d0575f2d309eabfc4afc4
ireneyiu <ireyiu@gmail.com>
2014-04-14 17:55:12 -0700

    Add semi-working solution for adding binary tree paths to sum

---
+++ trees-graphs/binary-tree-path-sum.rb
@@ -0,0 +1,44 @@
+=begin
+4.9 You are given a binary tree in which each node contains a value. Design an algorithm
+to print all paths which sum to a given value. The path does not need to
+start or end at the root or a leaf.
+=end
+
+class Node
+  attr_accessor :data, :left, :right
+  def initialize(data)
+    @data = data
+  end
+end
+
+def paths_by_sum(node, sum, path=[], level=0)
+  return unless node
+  p node.data
+  p path
+  path << node.data
+  total = 0
+  level.downto(0) do |i|
+    total += path[i]
+    p path[i..level] if total == sum
+  end
+
+  paths_by_sum(node.left, sum, path, level + 1)
+  paths_by_sum(node.right, sum, path, level + 1)
+
+  path[level] = 0
+end
+
+node4 = Node.new(4)
+node5 = Node.new(5)
+
+node1 = Node.new(1)
+node2 = Node.new(2)
+node2.left = node4
+node2.right = node5
+
+node5again = Node.new(5)
+
+node1.left = node2
+node1.right = node5again
+
+paths_by_sum(node1, 6)
